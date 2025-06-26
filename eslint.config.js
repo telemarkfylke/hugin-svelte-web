86e41d0ec27d72ccb8408a4522a26e9e8318cdf9
Rune Moskvil Lyngås <rune.moskvil.lyngas@vestfoldfylke.no>
2025-06-26 12:55:37 +0200

    fix: remove prettier integration from eslint config and update linting scripts

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,3 @@
-import prettier from 'eslint-config-prettier';
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
@@ -13,8 +12,6 @@ export default [
   includeIgnoreFile(gitignorePath),
   js.configs.recommended,
   ...svelte.configs.recommended,
-  prettier,
-  ...svelte.configs.prettier,
   {
     languageOptions: {
       globals: { ...globals.browser, ...globals.node }
